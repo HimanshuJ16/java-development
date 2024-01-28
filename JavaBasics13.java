ca99193b71427c27be48a1bfe992cca338d779b3
Himanshu Jangir <hjuberoi16@gmail.com>
2024-01-29 00:59:07 +0530

    OOPS

--- JavaBasics13.java
+++ JavaBasics13.java
@@ -1,20 +1,49 @@
 public class JavaBasics13 {
   public static void main(String[] args) {
+    // // code1
     Pen p1 = new Pen();
     p1.setColor("green");
-    System.out.println(p1.color);
+    System.out.println(p1.getColor());
+    p1.setTip(5);
+    System.out.println(p1.getTip());
+    p1.setColor("red");
+    System.out.println(p1.getColor());
+  
+    // // code2
+    // BankAccount myAcc = new BankAccount();
+    // myAcc.username = "himanshujangir";
+    // myAcc.setPassword("himorewww");
+
+    // // code3
+
   }  
 }
 
-class Pen {
-  String color;
-  int tip;
+class BankAccount {
+  public String username;
+  private String password;
+  public void setPassword(String pwd) {
+    password = pwd;
+  }
+}
 
-  void setColor(String newColor) {
-    color = newColor;
+class Pen {
+  private String color;
+  private int tip;
+
+  String getColor() {
+    return this.color;
   }
 
-  void setTip(int newTip) {
-    tip = newTip;
+  int getTip() {
+    return this.tip;
+  }
+
+  void setColor(String newColor) {
+    this.color = newColor;
+  }
+
+  void setTip(int tip) {
+    this.tip = tip;
   }
 }
