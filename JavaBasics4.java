7d25c7be3bbf1d85815b27acf09c1c564c07f056
Himanshu Jangir <hjuberoi16@gmail.com>
2024-01-28 14:31:28 +0530

    patterns_1

---
+++ JavaBasics4.java
@@ -0,0 +1,39 @@
+public class JavaBasics4 {
+  public static void main(String args[]) {
+    // // code1
+    // for(int line=1; line<=4; line++) {
+    //   for(int star=1; star<=line; star++) {
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
+
+    // // code2
+    // for(int line=1; line<=4; line++) {
+    //   for(int star=1; star<=4-line+1; star++) {
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
+
+    // // code3
+    // int n = 4;
+    // for(int line=1; line<=n; line++) {
+    //   for(int number = 1; number<=line; number++) {
+    //     System.out.print(number);
+    //   }
+    //   System.out.println();
+    // }
+
+    // // code4
+    // char ch = 'A';
+    // int n = 4;
+    // for(int i=1; i<=n; i++) {
+    //   for(int j=1; j<=i; j++) {
+    //     System.out.print(ch);
+    //     ch++;
+    //   }
+    //   System.out.println();
+    // }
+  }
+}
