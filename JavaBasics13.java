e47525e8ce63f49cdc65278160163b2fd5bb0432
Himanshu Jangir <hjuberoi16@gmail.com>
2024-01-29 01:30:47 +0530

    OOPS

--- JavaBasics13.java
+++ JavaBasics13.java
@@ -1,13 +1,13 @@
 public class JavaBasics13 {
   public static void main(String[] args) {
     // // code1
-    Pen p1 = new Pen();
-    p1.setColor("green");
-    System.out.println(p1.getColor());
-    p1.setTip(5);
-    System.out.println(p1.getTip());
-    p1.setColor("red");
-    System.out.println(p1.getColor());
+    // Pen p1 = new Pen();
+    // p1.setColor("green");
+    // System.out.println(p1.getColor());
+    // p1.setTip(5);
+    // System.out.println(p1.getTip());
+    // p1.setColor("red");
+    // System.out.println(p1.getColor());
   
     // // code2
     // BankAccount myAcc = new BankAccount();
@@ -15,8 +15,60 @@ public class JavaBasics13 {
     // myAcc.setPassword("himorewww");
 
     // // code3
-
+    // Student s1 = new Student();
+    // s1.name = "Himanshu";
+    // s1.roll = 546;
+    // s1.password = "abcd";
+    // s1.marks[0] = 100;
+    // s1.marks[1] = 90;
+    // s1.marks[2] = 80;
+    // Student s2 = new Student(s1);
+    // s2.password = "xys";
+    // s1.marks[2] = 100;
+    // for(int i=0; i<3; i++) {
+    //   System.out.println(s2.marks[i]);
+    // }
   }  
+}
+
+class Student {
+  String name;
+  int roll;
+  String password;
+  int marks[];
+
+  // // shallow copy constructor
+  // Student(Student s1) {
+  //   marks = new int[3];
+  //   this.name = s1.name;
+  //   this.roll = s1.roll;
+  //   this.marks = s1.marks;
+  // }
+
+  // // deep copy constructor
+  Student(Student s1) {
+    marks = new int[3];
+    this.name = s1.name;
+    this.roll = s1.roll;
+    for(int i=0; i<marks.length; i++) {
+      this.marks[i] = s1.marks[i];
+    }
+  }
+
+  Student() {
+    marks = new int[3];
+    System.out.println("constructor is called...");
+  }
+
+  Student(String name) {
+    marks = new int[3];
+    this.name = name;
+  }
+
+  Student(int roll) {
+    marks = new int[3];
+    this.roll = roll;
+  }
 }
 
 class BankAccount {
