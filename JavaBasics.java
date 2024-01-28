b1718a64c9862ee7ae694e215809a27595e58b64
Himanshu Jangir <hjuberoi16@gmail.com>
2024-01-28 14:22:48 +0530

    variables & data types

---
+++ JavaBasics.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class JavaBasics {
+  public static void main(String args[]) {
+
+    // code5
+    Scanner sc = new Scanner(System.in);
+    // String input = sc.next();
+    // System.out.println(input);
+
+    // String name = sc.nextLine();
+    // System.out.println(name);
+
+    // int number = sc.nextInt();
+    // System.out.println(number);
+
+    // float newnum = sc.nextFloat();
+    // System.out.println(newnum);
+
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int sum = a + b;
+    // int prod = a * b;
+    // System.out.println(sum);
+    // System.out.println(prod);
+
+    // float rad = sc.nextFloat();
+    // float area = 3.14f/*here f stands for float */ * rad * rad;
+    // System.out.println(area);
+
+    // float a = 12.25f;
+    // int b = (int) a;
+    // System.out.println(b);
+
+    // char ch = 'a';
+    // int b = ch;
+    // System.out.println(b);
+
+    // byte ch = 5;
+    // byte b = (byte) (ch * 2);
+    // System.out.println(b);
+
+    // // assignques1
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int c = sc.nextInt();
+    // int avg = (a + b + c) / 3;
+    // System.out.println("average is : " + avg);
+
+    // // assignques2
+    // int a = sc.nextInt();
+    // int area = a * a;
+    // System.out.println("area of the square is :" + area);
+
+    // // assignques3
+    // float pencil = sc.nextFloat();
+    // float pen = sc.nextFloat();
+    // float eraser = sc.nextFloat();
+    // float gst = (float) ((pencil + pen + eraser) * 0.18);
+    // float bill = (float) (pencil + pen + eraser + gst);
+    // System.out.println("Bill with 18% tax : " + bill);
+    sc.close();
+
+    // // code1
+    // System.out.print("Hello world!\nHello world!\n");
+    // System.out.println("Hello world!");
+    // System.out.println("Hello world!");
+
+    // // code2
+    // System.out.println("****");
+    // System.out.println("***");
+    // System.out.println("**");
+    // System.out.println("*");
+
+    // // code3
+    // int a = 10;
+    // int b = 5;
+    // System.out.println(a);
+    // System.out.println(b);
+    // String name = "Himanshu Jangir";
+    // System.out.println(name);
+    // a = b;
+    // System.out.println(a); 
+
+    // // code4
+    // int a = 15;
+    // int b = 41;
+    // int sum = a + b;
+    // System.out.println(sum);
+  }
+}
