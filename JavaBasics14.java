d56110c6975809abbcd896037b7d0f9b90026703
Himanshu Jangir <hjuberoi16@gmail.com>
2024-02-02 21:15:11 +0530

    recursion

--- JavaBasics14.java
+++ JavaBasics14.java
@@ -133,6 +133,57 @@ public class JavaBasics14 {
       printBinaryStrings(n-1, 1, str+"1");
     }
   }
+
+  public static void allOccurences(int arr[], int key, int i) {
+    if(i == arr.length) {
+      return;
+    } 
+    if(arr[i] == key) {
+      System.out.print(i + " ");
+    }
+    allOccurences(arr, key, i+1);
+  }
+
+  static String digits[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  public static void printDigits(int number) {
+    if(number == 0) {
+      return;
+    }
+    int lastDigit = number % 10;
+    printDigits(number / 10);
+    System.out.print(digits[lastDigit] + " ");
+  }
+
+  public static int length(String str) {
+    if(str.length() == 0) {
+      return 0;
+    }
+    return length(str.substring(1)) + 1;
+  }
+
+  public static int countSubstrs(String str, int i, int j, int n) {
+    if(n == 1) {
+      return 1;
+    }
+    if(n <= 0) {
+      return 0;
+    }
+    int res = countSubstrs(str, i+1, j, n-1) + countSubstrs(str, i, j-1, n-1) - countSubstrs(str, i+1, j-1, n-2);
+    if(str.charAt(i) == str.charAt(j)) {
+      res++;
+    }
+    return res; 
+  }
+
+  public static void towerOfHanoi(int n, String src, String helper, String dest) {
+    if(n == 1) {
+      System.out.println("transfer disk " + n + "from " + src + " to " + dest);
+      return;
+    }
+    towerOfHanoi(n-1, src, dest, helper);
+    System.out.println("transfer disk " + n + "from " + src + " to " + helper);
+    towerOfHanoi(n-1, helper, src, dest);
+  }
   public static void main(String[] args) {
     // // code1
     // int n = 25;
@@ -164,6 +215,27 @@ public class JavaBasics14 {
     // System.out.println(friendsPairing(3));
 
     // // code7
-    printBinaryStrings(3, 0, "");
+    // printBinaryStrings(3, 0, "");
+
+    // // assign1
+    // int arr[] = {3, 2, 4, 5, 6, 2, 7, 2, 2};
+    // int key = 2;
+    // allOccurences(arr, key, 0);
+
+    // // assign2
+    // printDigits(1234);
+
+    // // assign3
+    // String str = "abcde";
+    // System.out.println(length(str));
+
+    // // assign4
+    // String str = "abcab";
+    // int n = str.length();
+    // System.out.print(countSubstrs(str, 0, n-1, n));
+
+    // // assign5
+    int n = 4;
+    towerOfHanoi(n, "A", "B", "C");
   }
 }
