da551a9eb47950f81deca6ac72edebeeb03e5633
unknown <himanshujangir16@gmail.com>
2024-07-10 21:03:46 +0530

    Stacks_2

--- JavaBasics31.java
+++ JavaBasics31.java
@@ -27,8 +27,88 @@ public class JavaBasics31 {
       return false;
     }
   }
+
+  public static boolean isDuplicate(String str) {
+    Stack<Character> s = new Stack<>();
+
+    for(int i=0; i<str.length(); i++) {
+      char ch = str.charAt(i);
+
+      if (ch == ')') {
+        int count = 0;
+        while (s.peek() != '(') {
+          s.pop();
+          count++;
+        }
+        if (count < 1) {
+          return true;
+        } else {
+          s.pop();
+        }
+      } else {
+        s.push(ch);
+      }
+    }
+    return false;
+  }
+
+  public static void maxArea(int arr[]) {
+    int maxArea = 0;
+    int nsr[] = new int[arr.length];
+    int nsl[] = new int[arr.length];
+
+    //next smaller right
+    Stack<Integer> s = new Stack<>();
+
+    for(int i=arr.length-1; i>=0; i--) {
+      while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+        s.pop();
+      }
+      if (s.isEmpty()) {
+        nsr[i] = arr.length;
+      } else {
+        nsr[i] = s.peek();
+      }
+      s.push(i);
+    }
+
+    //next smaller left
+    s = new Stack<>();
+
+    for(int i=0; i<arr.length; i++) {
+      while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+        s.pop();
+      }
+      if (s.isEmpty()) {
+        nsl[i] = -1;
+      } else {
+        nsl[i] = s.peek();
+      }
+      s.push(i);
+    }
+
+    //current area
+    for(int i=0; i<arr.length; i++) {
+      int height = arr[i];
+      int width = nsr[i] - nsl[i] - 1;
+      int currArea = height * width;
+      maxArea = Math.max(currArea, maxArea);
+    }
+
+    System.out.println("max area in histogram = " + maxArea);
+  }
   public static void main(String[] args) {
-    String str = "({})[]";
-    System.out.println(isValid(str));
+    // CODE1
+    // String str = "({})[]";
+    // System.out.println(isValid(str));
+  
+    // CODE2
+    // String str = "((a+b))";
+    // String str2 = "(a-b)";
+    // System.out.println(isDuplicate(str));
+  
+    // CODE3
+    int arr[] = {2, 1, 5, 6, 2, 3};
+    maxArea(arr);
   }
 }
