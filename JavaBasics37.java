ebbec72bb1b4087b9f57822bda43d456b21941fb
unknown <himanshujangir16@gmail.com>
2024-07-16 01:01:07 +0530

    Greedy-Algorithms

--- JavaBasics37.java
+++ JavaBasics37.java
@@ -59,6 +59,18 @@ public class JavaBasics37 {
 
     System.out.println("the final value = " + finalVal);
   }
+
+  static class Job {
+    int deadline;
+    int profit;
+    int id;
+
+    public Job(int i, int d, int p) {
+      id = i;
+      deadline = d;
+      profit = p;
+    }
+  }
   public static void main(String[] args) {
     // CODE1
     int start[] = {1, 3, 0, 5, 8, 5};
@@ -71,6 +83,89 @@ public class JavaBasics37 {
     int W = 50;
     fractionalKnapsack(val, weight, W);   
     
-    // CODE3
+    // CODE3 - O(nlogn)
+    int A[] = {4, 1, 8, 7};
+    int B[] = {2, 3, 6, 5};
+
+    Arrays.sort(A);
+    Arrays.sort(B);
+
+    int minDiff = 0;
+
+    for(int i=0; i<A.length; i++) {
+      minDiff += Math.abs(A[i]-B[i]);
+    } 
+
+    System.out.println("min absolute diff of pairs = " + minDiff);
+  
+    // CODE4
+    int pairs[][] = {{5, 24}, {39, 60}, {5, 28}, {27, 40}, {50, 90}};
+
+    Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+    int chainLen = 1;
+    int chainEnd = pairs[0][1];
+
+    for(int i=1; i<pairs.length; i++) {
+      if(pairs[i][0] > chainEnd) {
+        chainLen++;
+        chainEnd = pairs[i][1];
+      }
+    }
+
+    System.out.println("max length of chain = " + chainLen);
+
+    // CODE5
+    Integer coins[] = {1, 2, 5, 10, 20, 50, 100, 200, 500};
+
+    Arrays.sort(coins, Comparator.reverseOrder());
+
+    int coutOfCoins = 0;
+    int amount = 590;
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    for(int i=0; i<coins.length; i++) {
+      if(coins[i] <= amount) {
+        while (coins[i] <= amount) {
+          coutOfCoins++;
+          ans.add(coins[i]);
+          amount -= coins[i];
+        }
+      }
+    }
+
+    System.out.println("total (min) coins used = " + coutOfCoins);
+
+    for(int i=0; i<ans.size(); i++) {
+      System.out.print(ans.get(i) + " ");
+    }
+    System.out.println();
+
+    // CODE6
+    int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+
+    ArrayList<Job> jobs = new ArrayList<>();
+
+    for(int i=0; i<jobsInfo.length; i++) {
+      jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+    }
+
+    Collections.sort(jobs, (a, b) -> b.profit-a.profit);
+
+    ArrayList<Integer> seq = new ArrayList<>();
+    int time = 0;
+    for(int i=0; i<jobs.size(); i++) {
+      Job curr = jobs.get(i);
+      if (curr.deadline > time) {
+        seq.add(curr.id);
+        time++;
+      }
+    }
+
+    System.out.println("max jobs = " + seq.size());
+    for(int i=0; i<seq.size(); i++) {
+      System.out.print(seq.get(i) + " ");
+    }
+    System.out.println();
   }
 }
