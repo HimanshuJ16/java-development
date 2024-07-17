ee6d5beb86c303cda3ab4a846ffacacc26194fff
unknown <himanshujangir16@gmail.com>
2024-07-17 18:12:51 +0530

    Binary-trees_1

--- JavaBasics39.java
+++ JavaBasics39.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class JavaBasics39 {
   static class Node {
     int data;
@@ -26,10 +28,117 @@ public class JavaBasics39 {
       return newNode;
     }
   }
+
+  public static void preOrder(Node root) {
+    if (root == null) {
+      return;
+    }
+    System.out.print(root.data + " ");
+    preOrder(root.left);
+    preOrder(root.right);
+  }
+
+  public static void inorder(Node root) {
+    if (root == null) {
+      return;
+    }
+    inorder(root.left);
+    System.out.print(root.data + " ");
+    inorder(root.right);
+  }
+
+  public static void postorder(Node root) {
+    if (root == null) {
+      return;
+    }
+    postorder(root.left);
+    postorder(root.right);
+    System.out.print(root.data + " ");
+  }
+
+  public static void levelOrder(Node root) {
+    if (root == null) {
+      return;
+    }
+
+    Queue<Node> q = new LinkedList<>();
+    q.add(root);
+    q.add(null);
+
+    while (!q.isEmpty()) {
+      Node currNode = q.remove();
+      if (currNode == null) {
+        System.out.println();
+        if (q.isEmpty()) {
+          break;
+        } else {
+          q.add(null);
+        }
+      } else {
+        System.out.print(currNode.data + " ");
+        if (currNode.left != null) {
+          q.add(currNode.left);
+        }
+        if (currNode.right != null) {
+          q.add(currNode.right);
+        }
+      }
+    }
+  }
+
+  public static int height(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int lh = height(root.left);
+    int rh = height(root.right);
+    return Math.max(lh, rh) + 1;
+  }
+
+  public static int count(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftCount = count(root.left);
+    int rightCount = count(root.right);
+    return leftCount + rightCount + 1;
+  }
+
+  public static int sum(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftsum = sum(root.left);
+    int rightsum = sum(root.right);
+    return leftsum + rightsum + root.data;
+  }
   public static void main(String[] args) {
-    int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-    BinaryTree tree = new BinaryTree();
-    Node root = tree.buildTree(nodes);
-    System.out.println(root.data);
+    // // CODE1
+    // int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    // BinaryTree tree = new BinaryTree();
+    // Node root = tree.buildTree(nodes);
+    // System.out.println(root.data);
+    // preOrder(root);
+    // System.out.println();
+    // inorder(root);
+    // System.out.println();
+    // postorder(root);
+    // System.out.println();
+    // levelOrder(root);
+
+    // // CODE2
+    Node root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
+    System.out.println(height(root));
+    System.out.println(count(root));
+    System.out.println(sum(root));
   }
 }
