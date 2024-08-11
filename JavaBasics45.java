d38356747174e79fcb9fb7b258c7acb5c4f55de9
unknown <himanshujangir16@gmail.com>
2024-08-11 13:36:20 +0530

    Heaps

--- JavaBasics45.java
+++ JavaBasics45.java
@@ -120,6 +120,59 @@ public class JavaBasics45 {
       heapify(arr, 0, i);
     }
   }
+
+  static class Point implements Comparable<Point> {
+    int x;
+    int y;
+    int distSq;
+    int idx;
+
+    public Point(int x, int y, int distSq, int idx) {
+      this.x = x;
+      this.y = y;
+      this.distSq = distSq;
+      this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(Point p2) {
+      return this.distSq - p2.distSq;
+    }
+  }
+
+  static class Row implements Comparable<Row> {
+    int soldiers;
+    int idx;
+
+    public Row(int soldiers, int idx) {
+      this.soldiers = soldiers;
+      this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(Row r2) {
+      if (this.soldiers == r2.soldiers) {
+        return this.idx - r2.idx;
+      } else {
+        return this.soldiers - r2.soldiers;
+      }
+    }
+  }
+
+  static class Pair implements Comparable<Pair> {
+    int val;
+    int idx;
+
+    public Pair(int val, int idx) {
+      this.val = val;
+      this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(Pair p2) {
+      return p2.val - this.val;
+    }
+  }
   public static void main(String[] args) {
     // // CODE1
     // PriorityQueue<Student> pq = new PriorityQueue<>();
@@ -147,11 +200,94 @@ public class JavaBasics45 {
     // }
 
     // // CODE3
-    int arr[] = {1, 2, 4, 5, 3};
-    heapSort(arr);
+    // int arr[] = {1, 2, 4, 5, 3};
+    // heapSort(arr);
 
-    for(int i=0; i<arr.length; i++) {
-      System.out.print(arr[i] + " ");
+    // for(int i=0; i<arr.length; i++) {
+    //   System.out.print(arr[i] + " ");
+    // }
+    // System.out.println();
+
+    // // CODE4
+    // int pts[][] = {{3, 3}, {5, -1}, {-2, 4}};
+    // int k = 2;
+
+    // PriorityQueue<Point> pq = new PriorityQueue<>();
+    // for(int i=0; i<pts.length; i++) {
+    //   int distSq = pts[i][0]*pts[i][0] + pts[i][1]*pts[i][1];
+    //   pq.add(new Point(pts[i][0], pts[i][1], distSq, i));
+    // }
+
+    // for(int i=0; i<k; i++) {
+    //   System.out.println("C" + pq.remove().idx);
+    // }
+
+    // // CODE5
+    // int ropes[] = {2, 3, 3, 4, 6};
+
+    // PriorityQueue<Integer> pq = new PriorityQueue<>();
+    // for(int i=0; i<ropes.length; i++) {
+    //   pq.add(ropes[i]);
+    // }
+
+    // int cost = 0;
+    // while (pq.size() > 1) {
+    //   int min = pq.remove();
+    //   int min2 = pq.remove();
+    //   cost += min + min2;
+    //   pq.add(min + min2);
+    // }
+
+    // System.out.println("cost of connecting n ropes = " + cost);
+  
+    // // CODE6
+    // int army[][] = {
+    //   {1, 0, 0, 0},
+    //   {1, 1, 1, 1},
+    //   {1, 0, 0, 0},
+    //   {1, 0, 0, 0}
+    // };
+
+    // int k = 2;
+
+    // PriorityQueue<Row> pq = new PriorityQueue<>();
+
+    // for(int i=0; i<army.length; i++) {
+    //   int count = 0;
+    //   for(int j=0; j<army[0].length; j++) {
+    //     count += army[i][j] == 1 ? 1 : 0;
+    //   }
+    //   pq.add(new Row(count, i));
+    // }
+
+    // for(int i=0; i<k; i++) {
+    //   System.out.println("R" + pq.remove().idx);
+    // }
+
+    // // CODE7 - O(nlogk)
+    int arr[] = {1, 3, -1, -3, 5, 3, 6, 7};
+    int k = 3;
+    int res[] = new int[arr.length-k+1];
+
+    PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+    for(int i=0; i<k; i++) {
+      pq.add(new Pair(arr[i], i));
+    }
+
+    res[0] = pq.peek().val;
+
+    for(int i=k; i<arr.length; i++) {
+      while (pq.size() > 0 && pq.peek().idx <= (i-k)) {
+        pq.remove();
+      }
+
+      pq.add(new Pair(arr[i], i));
+      res[i-k+1] = pq.peek().val;
+    }
+
+    for(int i=0; i<res.length; i++) {
+      System.out.print(res[i] + " ");
     }
     System.out.println();
   }
