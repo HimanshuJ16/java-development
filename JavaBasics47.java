112337eff97b0f02c61b76d2ab212b0b08cf0ee7
unknown <himanshujangir16@gmail.com>
2024-08-23 19:18:12 +0530

    Hashing

---
+++ JavaBasics47.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class JavaBasics47 {
+  public static void main(String[] args) {
+    // // CODE1
+    // //Create
+    // HashMap<String, Integer> hm = new HashMap<>();
+
+    // //Insert - O(1)
+    // hm.put("India", 100);
+    // hm.put("China", 150);
+    // hm.put("US", 50);
+    // System.out.println(hm);
+
+    // //Get - O(1)
+    // int population = hm.get("India");
+    // System.out.println(population);
+
+    // //ContainsKey - O(1)
+    // System.out.println(hm.containsKey("India"));
+
+    // //Remove - O(1)
+    // System.out.println(hm.remove("China"));
+    // System.out.println(hm);
+
+    // //Size
+    // System.out.println(hm.size());
+
+    // //Is Empty
+    // hm.clear();
+    // System.out.println(hm.isEmpty());
+
+    // // CODE2
+    // HashMap<String, Integer> hm = new HashMap<>();
+    // hm.put("India", 100);
+    // hm.put("China", 150);
+    // hm.put("US", 50);
+    // hm.put("Indonesia", 6);
+    // hm.put("Nepal", 5);
+
+    // Set<String> keys = hm.keySet();
+    // System.out.println(keys);
+
+    // for (String k : keys) {
+    //   System.out.println("key = " + k + ", " + "value = " + hm.get(k));
+    // }
+
+    // // CODE3
+    
+  }
+}
