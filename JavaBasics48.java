ee65dcb8320a13a97a816bf27e772c43f79e50e2
unknown <himanshujangir16@gmail.com>
2024-08-26 13:14:37 +0530

    Hashing_1

--- JavaBasics48.java
+++ JavaBasics48.java
@@ -1,18 +1,51 @@
 import java.util.*;
 
 public class JavaBasics48 {
-  public static void main(String[] args) {
-    int arr[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
-    HashMap<Integer, Integer> map = new HashMap<>();
-
-    for(int i=0; i<arr.length; i++) {
-      map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+  public static boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
     }
 
-    for (Integer key : map.keySet()) {
-      if (map.get(key) > arr.length/3) {
-        System.out.println(key);
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    for(int i=0; i<s.length(); i++) {
+      char ch = s.charAt(i);
+      map.put(ch, map.getOrDefault(ch, 0) + 1);
+    }
+
+    for(int i=0; i<t.length(); i++) {
+      char ch = t.charAt(i);
+      if (map.get(ch) != null) {
+        if (map.get(ch) == 1) {
+          map.remove(ch);
+        } else {
+          map.put(ch, map.get(ch)-1);
+        }
+      } else {
+        return false;
       }
     }
+
+    return map.isEmpty();
+  }
+  public static void main(String[] args) {
+    // // CODE1
+    // int arr[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+    // HashMap<Integer, Integer> map = new HashMap<>();
+
+    // for(int i=0; i<arr.length; i++) {
+    //   map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+    // }
+
+    // for (Integer key : map.keySet()) {
+    //   if (map.get(key) > arr.length/3) {
+    //     System.out.println(key);
+    //   }
+    // }
+
+    // // CODE2
+    // String s = "race";
+    // String t = "care";
+    // System.out.println(isAnagram(s, t));
   }
 }    
---
+++ JavaBasics49.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class JavaBasics49 {
+  public static void main(String[] args) {
+    // // CODE1
+    // HashSet<Integer> set = new HashSet<>();
+    // set.add(1);
+    // set.add(2);
+    // set.add(4);
+    // set.add(2);
+    // set.add(1);
+    // System.out.println(set);
+    // set.remove(2);
+    // System.out.println(set.contains(2));
+    // System.out.println(set.size());
+    // set.clear();
+    // System.out.println(set.isEmpty());
+
+    // // CODE2
+  }
+}
