5db886484ef643552df5b36fc0fa8ca6a444a59c
unknown <himanshujangir16@gmail.com>
2024-08-29 20:42:43 +0530

    Graphs

--- JavaBasics52.java
+++ JavaBasics52.java
@@ -12,51 +12,117 @@ public class JavaBasics52 {
       this.wt = w;
     }
   }
-  public static void main(String[] args) {
-    /*
-              (5)
-           0-------1
-                  / \
-             (1) /   \ (3)
-                /     \
-               2-------3 
-               |  (1)
-           (2) |
-               |
-               4 
-    */               
-          
-    int V = 5;
-    ArrayList<Edge>[] graph = new ArrayList[V];
 
-    for(int i=0; i<V; i++) {
+  static void createGraph(ArrayList<Edge> graph[]) {
+    for(int i=0; i<graph.length; i++) {
       graph[i] = new ArrayList<>();
     }
 
-    //0-vertex
-    graph[0].add(new Edge(0, 1, 5));
+    graph[0].add(new Edge(0, 1, 1));
+    graph[0].add(new Edge(0, 2, 1));
 
-    //1-vertex
-    graph[1].add(new Edge(1, 0, 5));
-    graph[1].add(new Edge(1, 2, 1));
-    graph[1].add(new Edge(1, 3, 3));
+    graph[1].add(new Edge(1, 0, 1));
+    graph[1].add(new Edge(1, 3, 1));
 
-    //2-vertex
-    graph[2].add(new Edge(2, 1, 1));
-    graph[2].add(new Edge(2, 3, 1));
-    graph[2].add(new Edge(2, 4, 2));
+    graph[2].add(new Edge(2, 0, 1));
+    graph[2].add(new Edge(2, 4, 1));
 
-    //3-vertex
-    graph[3].add(new Edge(3, 1, 3));
-    graph[3].add(new Edge(3, 2, 1));
+    graph[3].add(new Edge(3, 1, 1));
+    graph[3].add(new Edge(3, 4, 1));
+    graph[3].add(new Edge(3, 5, 1));
+    
+    graph[4].add(new Edge(4, 2, 1));
+    graph[4].add(new Edge(4, 3, 1));    
+    graph[4].add(new Edge(4, 5, 1));
+    
+    graph[5].add(new Edge(5, 3, 1));
+    graph[5].add(new Edge(5, 4, 1));
+    graph[5].add(new Edge(5, 6, 1));
+    
+    graph[6].add(new Edge(6, 5, 1));
+  }
 
-    //4-vertex
-    graph[4].add(new Edge(4, 2, 2));
+  public static void bfs(ArrayList<Edge>[] graph) {
+    Queue<Integer> q = new LinkedList<>();
+    boolean vis[] = new boolean[graph.length];
+    q.add(0);
 
-    //2's neighbours
-    for(int i=0; i<graph[2].size(); i++) {
-      Edge e = graph[2].get(i);
-      System.out.println(e.dest);
+    while (!q.isEmpty()) {
+      int curr = q.remove();
+
+      if (!vis[curr]) {
+        System.out.print(curr + " ");
+        vis[curr] = true;
+        for(int i=0; i<graph[curr].size(); i++) {
+          Edge e = graph[curr].get(i);
+          q.add(e.dest);
+        }
+      }
     }
+  }
+  public static void main(String[] args) {
+    // // CODE1
+    // /*
+    //           (5)
+    //        0-------1
+    //               / \
+    //          (1) /   \ (3)
+    //             /     \
+    //            2-------3 
+    //            |  (1)
+    //        (2) |
+    //            |
+    //            4 
+    // */               
+
+    // int V = 5;
+    // ArrayList<Edge>[] graph = new ArrayList[V];
+
+    // for(int i=0; i<V; i++) {
+    //   graph[i] = new ArrayList<>();
+    // }
+
+    // //0-vertex
+    // graph[0].add(new Edge(0, 1, 5));
+
+    // //1-vertex
+    // graph[1].add(new Edge(1, 0, 5));
+    // graph[1].add(new Edge(1, 2, 1));
+    // graph[1].add(new Edge(1, 3, 3));
+
+    // //2-vertex
+    // graph[2].add(new Edge(2, 1, 1));
+    // graph[2].add(new Edge(2, 3, 1));
+    // graph[2].add(new Edge(2, 4, 2));
+
+    // //3-vertex
+    // graph[3].add(new Edge(3, 1, 3));
+    // graph[3].add(new Edge(3, 2, 1));
+
+    // //4-vertex
+    // graph[4].add(new Edge(4, 2, 2));
+
+    // //2's neighbours
+    // for(int i=0; i<graph[2].size(); i++) {
+    //   Edge e = graph[2].get(i);
+    //   System.out.println(e.dest);
+    // }
+
+    // // CODE2
+    // /*
+    //         1-----3
+    //        /      | \
+    //       0       |  5-----6
+    //        \      | /
+    //         2-----4  
+    // */
+
+    // int V = 7;
+    // ArrayList<Edge> graph[] = new ArrayList[V];
+    // createGraph(graph);
+    // bfs(graph);
+
+    // // CODE3
+    
   }
 }
