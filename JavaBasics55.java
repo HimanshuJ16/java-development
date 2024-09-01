a800df9ddd60c88df75d265a60a464c36e93579f
unknown <himanshujangir16@gmail.com>
2024-09-01 20:23:49 +0530

    Graphs_2

--- JavaBasics55.java
+++ JavaBasics55.java
@@ -81,6 +81,21 @@ public class JavaBasics55 {
     graph[3].add(new Edge(3, 2, 50));
   }
 
+  static void createGraph4(int flights[][], ArrayList<Edge> graph[]) {
+    for(int i=0; i<graph.length; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    for(int i=0; i<flights.length; i++) {
+      int src = flights[i][0];
+      int dest = flights[i][1];
+      int wt = flights[i][2];
+
+      Edge e = new Edge(src, dest, wt);
+      graph[src].add(e);
+    }
+  }
+
   static class Pair implements Comparable<Pair> {
     int n;
     int path;
@@ -194,6 +209,58 @@ public class JavaBasics55 {
 
     System.out.println("final(min) cost of MST : " + finalCost);
   }
+
+  static class Info {
+    int v;
+    int cost;
+    int stops;
+
+    public Info(int v, int c, int s) {
+      this.v = v;
+      this.cost = c;
+      this.stops = s;
+    }
+  }
+
+  public static void cheapestFlight(int n, int flights[][], int src, int dest, int k) {
+    ArrayList<Edge> graph[] = new ArrayList[n];
+    createGraph4(flights, graph);
+
+    int dist[] = new int[n];
+    for(int i=0; i<n; i++) {
+      if (i != src) {
+        dist[i] = Integer.MAX_VALUE;
+      }
+    }
+
+    Queue<Info> q = new LinkedList<>();
+    q.add(new Info(src, 0, 0));
+
+    while (!q.isEmpty()) {
+      Info curr = q.remove();
+      if (curr.stops > k) {
+        break;
+      }
+
+      for(int i=0; i<graph[curr.v].size(); i++) {
+        Edge e = graph[curr.v].get(i);
+        int u = e.src;
+        int v = e.dest;
+        int wt = e.wt;
+
+        if (curr.cost + wt < dist[v] && curr.stops <= k) {
+          dist[v] = curr.cost + wt;
+          q.add(new Info(v, dist[v], curr.stops + 1));
+        }
+      }
+    }
+
+    if (dist[dest] == Integer.MAX_VALUE) {
+      System.out.println("-1");
+    } else {
+      System.out.println(dist[dest]);
+    }
+  }
   public static void main(String[] args) {
     // // CODE1
     // int V = 6;
@@ -214,5 +281,11 @@ public class JavaBasics55 {
     // ArrayList<Edge> graph[] = new ArrayList[V];
     // createGraph3(graph);
     // prims(graph);  
+
+    // // CODE4
+    int n = 4;
+    int flights[][] = {{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}};
+    int src = 0, dst = 3, k = 1;
+    cheapestFlight(n, flights, src, dst, k);
   }
 }
