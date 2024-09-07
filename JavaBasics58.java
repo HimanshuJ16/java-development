d1cc5234788124d14937ae8cc873c15146482b4f
unknown <himanshujangir16@gmail.com>
2024-09-07 09:05:03 +0530

    Dynamic_Programming

--- JavaBasics58.java
+++ JavaBasics58.java
@@ -241,6 +241,100 @@ public class JavaBasics58 {
     return dp[n][m];
   }
 
+  public static int longestCommonSubstring(String s1, String s2) {
+    int n = s1.length();
+    int m = s2.length();
+    int dp[][] = new int[n+1][m+1];
+    int ans = 0;
+    for(int i=0; i<n+1; i++) {
+      for(int j=0; j<m+1; j++) {
+        if(i==0 || j==0) {
+          dp[i][j] = 0;
+        } else if(s1.charAt(i-1) == s2.charAt(j-1)) {
+          dp[i][j] = 1 + dp[i-1][j-1];
+          ans = Math.max(ans, dp[i][j]);
+        } else {
+          dp[i][j] = 0;
+        }
+      }
+    }
+
+    print(dp);
+    return ans;
+  }
+
+  public static int lcs(int arr1[], int arr2[]) {
+    int n = arr1.length;
+    int m = arr2.length;
+    int dp[][] = new int[n+1][m+1];
+    for(int i=0; i<n+1; i++) {
+      for(int j=0; j<m+1; j++) {
+        if(i==0 || j==0) {
+          dp[i][j] = 0;
+        } else if(arr1[i-1] == arr2[j-1]) {
+          dp[i][j] = 1 + dp[i-1][j-1];
+        } else {
+          dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        }
+      }
+    }
+
+    print(dp);
+
+    return dp[n][m];
+  }
+
+  public static int lis(int arr[]) {
+    HashSet<Integer> set = new HashSet<>();
+    for(int i=0; i<arr.length; i++) {
+      set.add(arr[i]);
+    }
+
+    int lis[] = new int[set.size()];
+    int i = 0;
+    for(int num : set) {
+      lis[i] = num;
+      i++;
+    }
+
+    Arrays.sort(lis);
+
+    return lcs(arr, lis);
+  }
+
+  public static int minDistance(String word1, String word2) {
+    int n = word1.length();
+    int m = word2.length();
+    int dp[][] = new int[n+1][m+1];
+
+    for(int i=0; i<n+1; i++) {
+      for(int j=0; j<m+1; j++) {
+        if(i == 0) {
+          dp[i][j] = j;
+        }
+        if(j == 0) {
+          dp[i][j] = i;
+        }
+      }
+    }
+
+    for(int i=1; i<n+1; i++) {
+      for(int j=1; j<m+1; j++) {
+        if(word1.charAt(i-1) == word2.charAt(j-1)) {
+          dp[i][j] = dp[i-1][j-1];
+        } else {
+          int add = dp[i][j-1] + 1;
+          int del = dp[i-1][j] + 1;
+          int rep = dp[i-1][j-1] + 1;
+          dp[i][j] = Math.min(add, Math.min(del, rep));
+        }
+      }
+    }
+
+    print(dp);
+    return dp[n][m];
+  }
+
   public static void main(String[] args) {
     // // CODE1
     // int n = 5;
@@ -291,16 +385,33 @@ public class JavaBasics58 {
     // System.out.println(rodCutting(length, price, totRod));
   
     // // CODE8
-    String str1 = "abcdge";
-    String str2 = "abedg";
-    int dp[][] = new int[str1.length()+1][str2.length()+1];
-    for(int i=0; i<str1.length()+1; i++) {
-      for(int j=0; j<str2.length()+1; j++) {
-        dp[i][j] = -1;
-      }
-    }
-    System.out.println(lcs(str1, str2, str1.length(), str2.length()));
-    System.out.println(lcsMemoization(str1, str2, str1.length(), str2.length(), dp));
-    System.out.println(lcsTabulation(str1, str2, str1.length(), str2.length()));
+    // String str1 = "abcdge";
+    // String str2 = "abedg";
+    // int dp[][] = new int[str1.length()+1][str2.length()+1];
+    // for(int i=0; i<str1.length()+1; i++) {
+    //   for(int j=0; j<str2.length()+1; j++) {
+    //     dp[i][j] = -1;
+    //   }
+    // }
+    // System.out.println(lcs(str1, str2, str1.length(), str2.length()));
+    // System.out.println(lcsMemoization(str1, str2, str1.length(), str2.length(), dp));
+    // System.out.println(lcsTabulation(str1, str2, str1.length(), str2.length()));
+  
+    // // CODE9
+    // String str1 = "ABCDE";
+    // String str2 = "ABGCE";
+    // System.out.println(longestCommonSubstring(str1, str2));
+  
+    // // CODE10
+    // int arr[] = {50, 3, 10, 7, 40, 80};
+    // System.out.println(lis(arr));
+
+    // // CODE11
+    // String word1 = "intention";
+    // String word2 = "execution";
+    // System.out.println(minDistance(word1, word2));
+
+    // // CODE12
+
   }
 }
