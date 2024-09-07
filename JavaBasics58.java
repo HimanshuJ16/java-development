d752b33c64f550cf9a9adb05616e7fc0efc7c48e
unknown <himanshujangir16@gmail.com>
2024-09-07 13:36:09 +0530

    Dynamic_Programming

--- JavaBasics58.java
+++ JavaBasics58.java
@@ -335,6 +335,106 @@ public class JavaBasics58 {
     return dp[n][m];
   }
 
+  public static boolean isMatch(String s, String p) {
+    int n = s.length();
+    int m = p.length();
+
+    boolean dp[][] = new boolean[n+1][m+1];
+    dp[0][0] = true;
+
+    for(int i=1; i<n+1; i++) {
+      dp[i][0] = false;
+    }
+
+    for(int j=1; j<m+1; j++) {
+      if(p.charAt(j-1) == '*') {
+        dp[0][j] = dp[0][j-1];
+      }
+    }
+    
+    for(int i=1; i<n+1; i++) {
+      for(int j=1; j<m+1; j++) {
+        if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') {
+          dp[i][j] = dp[i-1][j-1];
+        } else if(p.charAt(j-1) == '*') {
+          dp[i][j] = dp[i][j-1] || dp[i-1][j];
+        } else {
+          dp[i][j] = false;
+        }
+      }
+    }
+
+    print1(dp);
+    return dp[n][m];
+  }
+
+  public static int catalanRecurion(int n) {
+    if(n <= 1) {
+      return 1;
+    }
+    int ans = 0;
+    for(int i=0; i<n; i++) {
+      ans += catalanRecurion(i) * catalanRecurion(n-i-1);
+    }
+    return ans;
+  }
+
+  public static int catalanMemoization(int n, int dp[]) {
+    if(n <= 1) {
+      return 1;
+    }
+    if(dp[n] != -1) {
+      return dp[n];
+    }
+    int ans = 0;
+    for(int i=0; i<n; i++) {
+      ans += catalanMemoization(i, dp) * catalanMemoization(n-i-1, dp);
+    }
+    return dp[n] = ans;
+  }
+
+  public static int catalanTabulation(int n) {
+    int dp[] = new int[n+1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i=2; i<=n; i++) {
+      int ans = 0;
+      for(int j=0; j<i; j++) {
+        ans += dp[j] * dp[i-j-1];
+      }
+      dp[i] = ans;
+    }
+    return dp[n];
+  }
+
+  public static int countBST(int n) {
+    int dp[] = new int[n+1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i=2; i<=n; i++) {
+      int ans = 0;
+      for(int j=0; j<i; j++) {
+        ans += dp[j] * dp[i-j-1];
+      }
+      dp[i] = ans;
+    }
+    return dp[n];
+  }
+
+  public static int mountainRanges(int n) {
+    int dp[] = new int[n+1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i=2; i<=n; i++) {
+      int ans = 0;
+      for(int j=0; j<i; j++) {
+        ans += dp[j] * dp[i-j-1];
+      }
+      dp[i] = ans;
+    }
+    return dp[n];
+  }
+
   public static void main(String[] args) {
     // // CODE1
     // int n = 5;
@@ -412,6 +512,24 @@ public class JavaBasics58 {
     // System.out.println(minDistance(word1, word2));
 
     // // CODE12
+    // String s = "abc";
+    // String p = "**c";
+    // System.out.println(isMatch(s, p));
 
+    // // CODE13
+    // int n = 5;
+    // int dp[] = new int[n+1];
+    // Arrays.fill(dp, -1);
+    // System.out.println(catalanRecurion(n));
+    // System.out.println(catalanMemoization(n, dp));
+    // System.out.println(catalanTabulation(n));
+
+    // // CODE14
+    // int n = 3;
+    // System.out.println(countBST(n));
+
+    // // CODE15
+    int n = 4;
+    System.out.println(mountainRanges(n));
   }
 }
